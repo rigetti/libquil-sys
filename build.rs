374d88998b9a691cba4db199fdbfae0180ae2751
marquessv <marquessavaldez@gmail.com>
2022-11-17 09:19:28 -0800

    make build dynamic

--- build.rs
+++ build.rs
@@ -2,18 +2,32 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    // TODO These shouldn't be tied to my fs.
+    // Get the quilc library path from users environment
+    // If unset, defaults to the standard directory for quicklisp local projects
+    let quilc_library_path = PathBuf::from(
+        env::var("QUILC_LIBRARY_PATH").unwrap_or(
+            format!(
+                "{}/quicklisp/local-projects/quilc/lib",
+                env::var("HOME").expect("$HOME should be set")
+            )
+            .to_string(),
+        ),
+    );
+    let quilc_library_path = quilc_library_path.to_string_lossy();
+
+    println!("{}", quilc_library_path);
     // Tell cargo to look for shared libraries in the specified directory
-    println!("cargo:rustc-link-search=/Users/mgsk/hackery/lisp/quilc/lib");
+    println!("cargo:rustc-link-search={}", quilc_library_path);
 
     println!("cargo:rustc-link-lib=quilc");
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
-    println!("cargo:rerun-if-changed=/Users/mgsk/hackery/lisp/quilc/lib/libquilc.h");
+    println!("cargo:rerun-if-changed={}/libquilc.h", quilc_library_path);
 
-    // TODO Condition this
-    // macos fix
-    println!("cargo:rustc-link-arg=-pagezero_size 0x100000");
+    //
+    if cfg!(target_os = "macos") {
+        println!("cargo:rustc-link-arg=-pagezero_size 0x100000");
+    }
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -21,19 +35,19 @@ fn main() {
     let bindings = bindgen::Builder::default()
         // The input header we would like to generate
         // bindings for.
-        .header("/Users/mgsk/hackery/lisp/quilc/lib/libquilc.h")
+        .header(format!("{}/libquilc.h", quilc_library_path))
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
+        .expect("Bindings should be generated");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     println!("Writing bindings to {}", out_path.to_string_lossy());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+        .expect("Should be able to write bindings to file.");
 }
