8ec2acf5bbf0bab607c576ccf87360fcca54005f
Mark Skilbeck <mark.skilbeck@rigetti.com>
2023-07-19 19:47:00 +0100

    support arbitrary chips from JSON
    and rework some other stuff to be more rusty

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,11 @@
 
 use std::{
     ffi::{CStr, CString},
+    fmt::Display,
     sync::Once,
 };
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
-/// A quilc chip specification
-#[derive(Debug)]
-pub struct Chip(chip_specification);
-
-/// A parsed Quil program
-#[derive(Debug)]
-pub struct Program(quil_program);
 
 static START: Once = Once::new();
 
@@ -31,23 +24,65 @@ fn init_libquilc() {
     })
 }
 
-/// Parses a [`CString`] into a [`Program`] for use with other libquil calls.
-pub fn parse_program(program: CString) -> Program {
-    init_libquilc();
-    let mut c_chars: Vec<i8> = program
-        .as_bytes_with_nul()
-        .to_vec()
-        .iter()
-        .map(|c| *c as i8)
-        .collect();
-    let ptr = c_chars.as_mut_ptr();
-    let mut parsed_program: quil_program = std::ptr::null_mut();
+/// A quilc chip specification
+#[derive(Debug)]
+pub struct Chip(chip_specification);
 
-    unsafe {
-        quilc_parse_quil.unwrap()(ptr, &mut parsed_program);
+impl From<CString> for Chip {
+    fn from(json: CString) -> Self {
+        init_libquilc();
+
+        let mut c_chars: Vec<i8> = json
+            .as_bytes_with_nul()
+            .to_vec()
+            .iter()
+            .map(|c| *c as i8)
+            .collect();
+        let ptr = c_chars.as_mut_ptr();
+
+        unsafe {
+            let mut chip: chip_specification = std::ptr::null_mut();
+            quilc_parse_chip_spec_isa_json.unwrap()(ptr, &mut chip);
+            Chip(chip)
+        }
     }
+}
 
-    Program(parsed_program)
+/// A parsed Quil program
+#[derive(Debug)]
+pub struct Program(quil_program);
+
+impl From<CString> for Program {
+    fn from(program: CString) -> Self {
+        init_libquilc();
+        let mut c_chars: Vec<i8> = program
+            .as_bytes_with_nul()
+            .to_vec()
+            .iter()
+            .map(|c| *c as i8)
+            .collect();
+        let ptr = c_chars.as_mut_ptr();
+        let mut parsed_program: quil_program = std::ptr::null_mut();
+
+        unsafe {
+            quilc_parse_quil.unwrap()(ptr, &mut parsed_program);
+        }
+
+        Program(parsed_program)
+    }
+}
+
+impl Display for Program {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        init_libquilc();
+
+        unsafe {
+            let mut program_string_ptr: *mut std::os::raw::c_char = std::ptr::null_mut();
+            quilc_program_string.unwrap()(self.0, &mut program_string_ptr);
+            let program_string = CStr::from_ptr(program_string_ptr).to_str().unwrap();
+            write!(f, "{program_string}")
+        }
+    }
 }
 
 /// Compiles the [`Program`] for the given [`Chip`]
@@ -95,17 +130,6 @@ pub fn print_program(program: &Program) {
     }
 }
 
-/// Get the [`CString`] representation of a program
-pub fn program_string(program: &Program) -> CString {
-    init_libquilc();
-
-    unsafe {
-        let mut program_string_ptr: *mut std::os::raw::c_char = std::ptr::null_mut();
-        quilc_program_string.unwrap()(program.0, &mut program_string_ptr);
-        CStr::from_ptr(program_string_ptr).into()
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -122,7 +146,7 @@ MEASURE 1 ro[1]
 ";
 
     fn new_quil_program() -> Program {
-        parse_program(CString::new(sample_quil).unwrap())
+        CString::new(sample_quil).unwrap().into()
     }
 
     #[test]
@@ -136,11 +160,7 @@ MEASURE 1 ro[1]
     fn test_program_string() {
         let expected: quil_rs::Program = sample_quil.parse().unwrap();
         let program = new_quil_program();
-        let actual: quil_rs::Program = program_string(&program)
-            .into_string()
-            .unwrap()
-            .parse()
-            .unwrap();
+        let actual: quil_rs::Program = program.to_string().parse().unwrap();
         assert_eq!(actual, expected);
     }
 }
