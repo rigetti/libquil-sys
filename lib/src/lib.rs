32e7162ab59753cf3fed5f6aa1805df5e79f3a32
Mark Skilbeck <mark.skilbeck@rigetti.com>
2023-08-02 16:58:12 +0100

    Many changes...
    * improves error support
    * improves libquilc.core loading
    * fixes libquilc.so missing symbols
    * adds lib tests
    * renames python module to libquil_py

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -4,24 +4,61 @@
 
 use std::{
     ffi::{CStr, CString},
-    fmt::Display,
+    path::Path,
+    str::FromStr,
     sync::Once,
 };
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("error when calling quilc_compile_quil: {0}")]
+    CompileQuil(String),
+    #[error("error when calling quilc_compile_protoquil: {0}")]
+    CompileProtoquil(String),
+    #[error("error when calling quilc_parse_quil: {0}")]
+    ParseQuil(String),
+    #[error("program string contained unexpected NUL character: {0}")]
+    UnexpectedNul(#[from] std::ffi::NulError),
+    #[error("error when calling quilc_build_nq_linear_chip: {0}")]
+    BuildNqLinearChip(String),
+    #[error("error when calling quilc_parse_chip_spec_isa_json: {0}")]
+    ParseChip(String),
+    #[error("error when calling quilc_print_program: {0}")]
+    PrintProgram(String),
+    #[error("error when calling quilc_program_string: {0}")]
+    ProgramString(String),
+    #[error("invalid UTF-8 program: {0}")]
+    ProgramUtf8(#[from] std::str::Utf8Error),
+}
+
 static START: Once = Once::new();
 
 /// Initializes libquilc using it's core image. No-op after the first call.
 fn init_libquilc() {
-    let bytes = b"libquilc.core\0".to_vec();
-    let mut c_chars: Vec<i8> = bytes.iter().map(|c| *c as i8).collect();
-    let ptr = c_chars.as_mut_ptr();
-    unsafe {
-        dbg!("before");
-        init(ptr);
-        dbg!("after");
-    }
+    START.call_once(|| {
+        let path = match std::env::var("LIBQUILC_CORE_PATH") {
+            Ok(path) => path,
+            Err(_) => "libquilc.core".to_string(),
+        };
+        if !Path::new(&path).exists() {
+            // TODO Make this an error rather than a panic
+            panic!("Could not find libquilc core file. Do you need to set LIBQUILC_CORE_PATH environment variable?");
+        }
+        let bytes = match std::env::var("LIBQUILC_CORE_PATH") {
+            Ok(mut path) => {
+                path.push('\0');
+                path.as_bytes().to_vec()
+            }
+            Err(_) => b"libquilc.core\0".to_vec(),
+        };
+        let mut c_chars: Vec<i8> = bytes.iter().map(|c| *c as i8).collect();
+        let ptr = c_chars.as_mut_ptr();
+        unsafe {
+            init(ptr);
+        }
+    })
 }
 
 /// A quilc chip specification
@@ -30,8 +67,10 @@ pub struct Chip(chip_specification);
 
 unsafe impl Send for Chip {}
 
-impl From<CString> for Chip {
-    fn from(json: CString) -> Self {
+impl TryFrom<CString> for Chip {
+    type Error = Error;
+
+    fn try_from(json: CString) -> Result<Self, Self::Error> {
         init_libquilc();
 
         let mut c_chars: Vec<i8> = json
@@ -41,24 +80,34 @@ impl From<CString> for Chip {
             .map(|c| *c as i8)
             .collect();
         let ptr = c_chars.as_mut_ptr();
+        let mut chip: chip_specification = std::ptr::null_mut();
 
         unsafe {
-            let mut chip: chip_specification = std::ptr::null_mut();
-            quilc_parse_chip_spec_isa_json.unwrap()(ptr, &mut chip);
-            Chip(chip)
+            let err = quilc_parse_chip_spec_isa_json.unwrap()(ptr, &mut chip);
+            handle_libquil_error(err).map_err(Error::ParseChip)?;
         }
+
+        Ok(Chip(chip))
     }
 }
 
+impl FromStr for Chip {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        CString::new(s).map_err(Error::UnexpectedNul)?.try_into()
+    }
+}
 /// A parsed Quil program
 #[derive(Clone, Debug)]
 pub struct Program(quil_program);
 
 unsafe impl Send for Program {}
 
-impl From<CString> for Program {
-    fn from(program: CString) -> Self {
-        dbg!("hello");
+impl TryFrom<CString> for Program {
+    type Error = Error;
+
+    fn try_from(program: CString) -> Result<Self, Self::Error> {
         init_libquilc();
         let mut c_chars: Vec<i8> = program
             .as_bytes_with_nul()
@@ -69,104 +118,163 @@ impl From<CString> for Program {
         let ptr = c_chars.as_mut_ptr();
         let mut parsed_program: quil_program = std::ptr::null_mut();
 
-        dbg!(&c_chars);
         unsafe {
-            quilc_parse_quil.unwrap()(ptr, &mut parsed_program);
+            let err = quilc_parse_quil.unwrap()(ptr, &mut parsed_program);
+            handle_libquil_error(err).map_err(Error::ParseQuil)?;
         }
 
-        Program(parsed_program)
+        Ok(Program(parsed_program))
     }
 }
 
-impl Display for Program {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl FromStr for Program {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        CString::new(s).map_err(Error::UnexpectedNul)?.try_into()
+    }
+}
+
+impl Program {
+    pub fn to_string(&self) -> Result<String, Error> {
         init_libquilc();
 
         unsafe {
             let mut program_string_ptr: *mut std::os::raw::c_char = std::ptr::null_mut();
-            quilc_program_string.unwrap()(self.0, &mut program_string_ptr);
-            let program_string = CStr::from_ptr(program_string_ptr).to_str().unwrap();
-            write!(f, "{program_string}")
+            let err = quilc_program_string.unwrap()(self.0, &mut program_string_ptr);
+            handle_libquil_error(err).map_err(Error::ProgramString)?;
+            let program_string = CStr::from_ptr(program_string_ptr).to_str()?;
+            Ok(program_string.to_string())
         }
     }
 }
 
 /// Compiles the [`Program`] for the given [`Chip`]
-pub fn compile_program(program: &Program, chip: &Chip) -> Program {
+pub fn compile_program(program: &Program, chip: &Chip) -> Result<Program, Error> {
     init_libquilc();
     let mut compiled_program: quil_program = std::ptr::null_mut();
 
     unsafe {
-        quilc_compile_quil.unwrap()(program.0, chip.0, &mut compiled_program);
+        let err = quilc_compile_quil.unwrap()(program.0, chip.0, &mut compiled_program);
+        handle_libquil_error(err).map_err(Error::CompileQuil)?;
     }
 
-    Program(compiled_program)
+    Ok(Program(compiled_program))
 }
 
 /// Compiles the [`Program`] for the given [`Chip`] and restricts
 /// the resulting [`Program`] to satisfy "protoquil" constraints
-pub fn compile_protoquil(program: &Program, chip: &Chip) -> Program {
+pub fn compile_protoquil(program: &Program, chip: &Chip) -> Result<Program, Error> {
     init_libquilc();
     let mut compiled_program: quil_program = std::ptr::null_mut();
 
     unsafe {
-        quilc_compile_protoquil.unwrap()(program.0, chip.0, &mut compiled_program);
+        let err = quilc_compile_protoquil.unwrap()(program.0, chip.0, &mut compiled_program);
+        handle_libquil_error(err).map_err(Error::CompileProtoquil)?;
     }
 
-    Program(compiled_program)
+    Ok(Program(compiled_program))
 }
 
 /// Get a fully-connected 2Q [`Chip`]
-pub fn get_chip() -> Chip {
+// TODO Remove in favor of a better chip builder
+pub fn get_chip() -> Result<Chip, Error> {
     init_libquilc();
     let mut chip: chip_specification = std::ptr::null_mut();
 
     unsafe {
-        quilc_build_nq_linear_chip.unwrap()(2, &mut chip);
+        let err = quilc_build_nq_linear_chip.unwrap()(2, &mut chip);
+        handle_libquil_error(err).map_err(Error::BuildNqLinearChip)?;
     }
 
-    Chip(chip)
+    Ok(Chip(chip))
 }
 
 /// Prints the given [`Program`] to stdout
-pub fn print_program(program: &Program) {
+pub fn print_program(program: &Program) -> Result<(), Error> {
     init_libquilc();
+
     unsafe {
-        quilc_print_program.unwrap()(program.0);
+        let err = quilc_print_program.unwrap()(program.0);
+        handle_libquil_error(err).map_err(Error::PrintProgram)?;
+    }
+
+    Ok(())
+}
+
+fn handle_libquil_error(errno: error_t) -> Result<(), String> {
+    if errno == error_t_ERROR_SUCCESS {
+        return Ok(());
+    }
+
+    let mut error_str_ptr: *mut std::os::raw::c_char = std::ptr::null_mut();
+
+    unsafe {
+        let err = quilc_error.unwrap()(&mut error_str_ptr);
+        if err != 0 {
+            return Err("unknown error occurred".to_string());
+        }
+        let error_str = CStr::from_ptr(error_str_ptr).to_str().unwrap();
+        Err(error_str.to_string())
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use assert2::let_assert;
 
     const sample_quil: &str = "DECLARE ro BIT[2]
 DECLARE theta REAL
 RX(theta) 0
-X 0
 CNOT 0 1
-
 
 MEASURE 0 ro[0]
 MEASURE 1 ro[1]
 ";
 
     fn new_quil_program() -> Program {
-        CString::new(sample_quil).unwrap().into()
+        CString::new(sample_quil).unwrap().try_into().unwrap()
+    }
+
+    #[test]
+    fn test_program_parse_error() {
+        let_assert!(Error::ParseQuil(error) = Program::from_str("X 0\n    Y 0").err().unwrap());
+        assert!(error.contains("unexpected token of type :INDENT"));
+    }
+
+    #[test]
+    fn test_program_compilation_error() {
+        // Program should parse correctly, but compilation should fail
+        // due to the unrecognized instruction
+        let program = Program::from_str("GATE").unwrap();
+        let_assert!(
+            Error::CompileQuil(error) = compile_program(&program, &get_chip().unwrap())
+                .err()
+                .unwrap()
+        );
+        assert!(error.contains("Unrecognized instruction"));
+
+        let_assert!(
+            Error::CompileProtoquil(error) = compile_protoquil(&program, &get_chip().unwrap())
+                .err()
+                .unwrap()
+        );
+        assert!(error.contains("Unrecognized instruction"));
     }
 
     #[test]
     fn test_compile_protoquil() {
         let program = new_quil_program();
-        let chip = get_chip();
-        compile_protoquil(&program, &chip);
+        let chip = get_chip().unwrap();
+        compile_protoquil(&program, &chip).unwrap();
     }
 
     #[test]
     fn test_program_string() {
         let expected: quil_rs::Program = sample_quil.parse().unwrap();
         let program = new_quil_program();
-        let actual: quil_rs::Program = program.to_string().parse().unwrap();
+        let actual: quil_rs::Program = program.to_string().unwrap().parse().unwrap();
         assert_eq!(actual, expected);
     }
 }
--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,13 +1,25 @@
 use pyo3::prelude::*;
 use pyo3::types::PyString;
-use std::ffi::CString;
 
-use rigetti_pyo3::{create_init_submodule, py_wrap_struct, PyWrapper};
+use rigetti_pyo3::{
+    create_init_submodule, py_wrap_error, py_wrap_struct, wrap_error, PyWrapper, ToPythonError,
+};
 
 create_init_submodule! {
     classes: [PyChip, PyProgram],
     funcs: [compile],
 }
+
+wrap_error! {
+    RustLibquilError(libquil_sys::Error)
+}
+
+py_wrap_error!(
+    libquil_py,
+    RustLibquilError,
+    PyLibquilError,
+    pyo3::exceptions::PyException
+);
 
 #[derive(Clone)]
 pub struct Chip(libquil_sys::Chip);
@@ -16,8 +28,10 @@ py_wrap_struct! {
     PyChip(Chip) as "Chip" {
         py -> rs {
             str: Py<PyString> => Chip {
-                let cstr = CString::new(str.as_ref(py).to_str()?)?;
-                Ok::<_, PyErr>(Chip(cstr.into()))
+                let s = str.as_ref(py).to_str()?;
+                let lc: libquil_sys::Chip = s.parse().map_err(|err: libquil_sys::Error| RustLibquilError::from(err).to_py_err())?;
+                let c: Chip = Chip(lc);
+                Ok::<_, PyErr>(c)
             }
         },
     }
@@ -30,23 +44,38 @@ py_wrap_struct! {
     PyProgram(Program) as "Program" {
         py -> rs {
             str: Py<PyString> => Chip {
-                let cstr = CString::new(str.as_ref(py).to_str()?)?;
-                dbg!(&cstr);
-                Ok::<_, PyErr>(Program(cstr.into()))
+                let s = str.as_ref(py).to_str()?;
+                let program = s.parse().map_err(|err: libquil_sys::Error| RustLibquilError::from(err).to_py_err())?;
+                Ok::<_, PyErr>(Program(program))
             }
         },
     }
 }
 
 #[pymodule]
-fn libquil(py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    init_submodule("libquil", py, m)?;
+fn libquil_py(py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    unsafe {
+        // The library built by maturin does link to libquilc, but
+        // the linker does not make the libquilc symbols available
+        // to the lisp image. To get around that, we load it here
+        // with the `RTLD_GLOBAL` flag which makes symbols available
+        // to the whole process.
+        libloading::os::unix::Library::open(
+            Some("libquilc.so"),
+            libloading::os::unix::RTLD_NOW | libloading::os::unix::RTLD_GLOBAL,
+        )
+        .unwrap();
+    }
+    init_submodule("libquil_py", py, m)?;
     Ok(())
 }
 
 #[pyfunction]
 pub fn compile(program: PyProgram, chip: PyChip) -> PyResult<String> {
     let compiled_program =
-        libquil_sys::compile_program(&program.into_inner().0, &chip.into_inner().0);
-    Ok(format!("{compiled_program}"))
+        libquil_sys::compile_program(&program.into_inner().0, &chip.into_inner().0)
+            .map_err(|e| RustLibquilError::from(e).to_py_err())?;
+    compiled_program
+        .to_string()
+        .map_err(|e| RustLibquilError::from(e).to_py_err())
 }
